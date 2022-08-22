901ebf472c4fe6c8eebd5e488c63624b6d64d9f7
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2022-08-22 18:20:21 +0200

    Update test_utf8_excel.rb
    ruby test_utf8_excel.rb

--- test_utf8_excel.rb
+++ test_utf8_excel.rb
@@ -1,6 +1,16 @@
+require 'caxlsx'
 require 'simple_xlsx_reader'
+
+Axlsx::Package.new do |p|
+  p.workbook.add_worksheet(:name => "Testsheet") do |sheet|
+    sheet.add_row ['Test', 'Umläut', 'Täst']
+    sheet.add_row [1, 2, 3]
+  end
+  p.serialize('test.xlsx')
+end
 
 doc = SimpleXlsxReader.open('test.xlsx')
 puts doc.sheets.first.name # 'Sheet1'
 puts doc.sheets.first.rows.first.join(', ')
+
 puts doc.sheets.first.rows.first == ['Test', 'Umläute', 'Täst']
